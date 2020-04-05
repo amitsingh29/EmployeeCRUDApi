a7d08a18b951cce38cbf6ed64f64411141641270
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-05 22:46:30 +0530

    [Amit] Add. Adding UserDBContext and EmployeeRepository

---
+++ Repository/DBContext/UserDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.DBContext
+{
+    public class UserDBContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDBContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the employees.
+        /// </summary>
+        /// <value>
+        /// The employees.
+        /// </value>
+        public DbSet<EmployeeModel> Employees
+        {
+            get; set;
+        }
+    }
+}
