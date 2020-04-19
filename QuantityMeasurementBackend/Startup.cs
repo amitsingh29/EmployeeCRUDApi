4a7899ffa8ee1d5c05cd88a3226017efbe631f21
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-20 02:10:56 +0530

    [Amit] Refactor. Adding Swagger login in Startup.cs

--- QuantityMeasurementBackend/Startup.cs
+++ QuantityMeasurementBackend/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Manager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Repository;
 
 namespace QuantityMeasurementBackend
 {
@@ -26,6 +29,14 @@ namespace QuantityMeasurementBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient<IManager, QuantityManager>();
+            services.AddTransient<IRepository, QuantityRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "QuantiyMeasurement_Backend", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +45,12 @@ namespace QuantityMeasurementBackend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyApi V1");
+                });
+
             }
             else
             {
@@ -41,7 +58,14 @@ namespace QuantityMeasurementBackend
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=ValuesController}/{action=FeetToInch}/{id?}"
+                    );
+            });
         }
     }
 }
