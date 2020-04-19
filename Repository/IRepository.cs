822fc9c7763f25ec53a749fea715971435ad9aaf
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-20 00:30:02 +0530

    [Amit] Add. Adding QuantityModel.cs in Model

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository
 {
-    interface IRepository
+    public interface IRepository
     {
+        decimal FeetToInch(QuantityModel value);
+
+        decimal InchToFeet(QuantityModel value);
+
+        decimal MetertoCentimeter(QuantityModel value);
+
+        decimal CentimetertoMeter(QuantityModel value);
+
+        decimal KilogramToGram(QuantityModel value);
+
+        decimal GramToKilogram(QuantityModel value);
     }
 }
