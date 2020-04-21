8d5f3518ccd1138e00337d05af954bb42df9f64d
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-21 06:10:01 +0530

    [Amit] Add. Adding QuantityType component in Conversion

---
+++ src/Components/Conversions/QuantityType.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class QuantityType extends Component {
+    
+    render() {
+        return (
+            <select name="quantityType" id="quantityType" className=' form-control'  onChange={this.props.measurementTypeChanged}>
+                <option value="-1" selected>Select</option>
+                <option name="length">Length</option>
+                <option name="weight">Weight</option>
+            </select>
+        )
+    }
+}
+export default QuantityType;
