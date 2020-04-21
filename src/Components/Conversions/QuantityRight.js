429043e47fe9f5aab005b91282f2a733204d3602
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-21 06:27:40 +0530

    [Amit] Add. Adding QuantityRight component in Components

---
+++ src/Components/Conversions/QuantityRight.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+class QuantityRight extends Component{
+    render(){
+        return(
+            <div className='col-md m-5' id='main-label'>
+                <input type='text' className="form-control form-control-md" onChange={this.props.inputTypeRight} value={this.props.quantityRight} />
+                <select name="quantityType" id="quantityType" className='form-control'  onChange={this.props.selectQuantityRight}>
+                <option value="-1" selected>Select</option>
+                
+                {
+                    this.props.showType?
+                    <>
+                <option name="length">Feet</option>
+                <option name="weight">Inch</option>
+                <option name="weight">Meter</option>
+                <option name="weight">Centimeter</option></> : <>
+                <option name="weight">Kilogram</option>
+                <option name="weight">Gram</option>
+                </>
+                }
+                </select>
+            </div>
+        )
+    }
+}
+export default QuantityRight;
