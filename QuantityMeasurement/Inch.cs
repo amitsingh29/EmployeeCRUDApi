76d15759e3726cbb7699c35b155fada37437eb73
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-30 20:50:49 +0530

    [Amit] Add. Adding Inch.cs in QuantityMeasurement

---
+++ QuantityMeasurement/Inch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class Inch
+    {
+        private int inch;
+        public Inch()
+        {
+        }
+
+        public Inch(int inch)
+        {
+            this.inch = inch;
+        }
+
+        public int CheckInch_Value()
+        {
+            return this.inch;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+
+    }
+}
