d95899820933d31299c0608005412e27b7cf055a
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-04-30 22:00:50 +0530

    [Amit] Refactor. Added login in Gram class

--- QuantityMeasurement/Gram.cs
+++ QuantityMeasurement/Gram.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace QuantityMeasurement
 {
-    class Gram
+    public class Gram
     {
+        public int gram;
+
+        public Gram()
+        {
+        }
+
+        public Gram(int gram)
+        {
+            this.gram = gram;
+        }
+
+        public int EqualsValue()
+        {
+            return this.gram;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+
     }
 }
